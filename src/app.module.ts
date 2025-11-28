f93c475dc4fc378c03b83e8e632ae90b47f050d3
Evanson Igiri <evansonigiri@Evansons-MacBook-Air.local>
2025-11-28 15:12:40 +0100

    Add Prisma ORM with PostgreSQL database schema, migrations, and seed data

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HealthModule } from './health/health.module';
 import { AuthModule } from './auth/auth.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [HealthModule, AuthModule],
+  imports: [PrismaModule, HealthModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
